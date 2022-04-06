cc47616df496ec78a3e35f22366e0da26744808d
Yoga Wibowo <ywibowo@student.ethz.ch>
2022-04-06 22:57:19 +0200

    edit location of checkppoint 1 config

--- configs/panformer/panformer_pvtb5_24e_coco_panoptic.py
+++ configs/panformer/panformer_pvtb5_24e_coco_panoptic.py
@@ -4,7 +4,7 @@ _base_ = './base.py'
 model = dict(
     # get pvt_v2_b5_22k 
     # wget https://github.com/whai362/PVT/releases/download/v2/pvt_v2_b5_22k.pth
-    pretrained='./checkpoints/pvt_v2_b5_22k.pth',
+    pretrained='./work_dirs/models/pvt_v2_b5_22k.pth',
     backbone=dict(
         type='pvt_v2_b5',
         out_indices=(1, 2, 3),
