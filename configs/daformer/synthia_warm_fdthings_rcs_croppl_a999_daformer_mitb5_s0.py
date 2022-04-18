434fa6272cb15310d7f8fc35e5cdde09ec1bc624
Yoga Wibowo <ywibowo@student.ethz.ch>
2022-04-18 11:13:52 +0200

    add synthia 2 cs config file

---
+++ configs/daformer/synthia_warm_fdthings_rcs_croppl_a999_daformer_mitb5_s0.py
@@ -0,0 +1,42 @@
+_base_ = [
+    '../_base_/default_runtime.py',
+    # DAFormer Network Architecture
+    '../_base_/models/daformer_sepaspp_mitb5.py',
+    # Synthia->Cityscapes Data Loading
+    '../_base_/datasets/synthia_512x512.py',
+    # AdamW Optimizer
+    '../_base_/schedules/adamw.py',
+    # Linear Learning Rate Warmup with Subsequent Linear Decay
+    '../_base_/schedules/poly10warm.py'
+]
+# Random Seed
+seed = 0
+# Modifications to Basic UDA
+data = dict(
+    train=dict(
+        # Rare Class Sampling
+        rare_class_sampling=dict(
+            min_pixels=3000, class_temp=0.01, min_crop_ratio=0.5)))
+# Optimizer Hyperparameters
+optimizer_config = None
+optimizer = dict(
+    lr=6e-05,
+    paramwise_cfg=dict(
+        custom_keys=dict(
+            head=dict(lr_mult=10.0),
+            pos_block=dict(decay_mult=0.0),
+            norm=dict(decay_mult=0.0))))
+n_gpus = 1
+runner = dict(type='IterBasedRunner', max_iters=40000)
+# Logging Configuration
+checkpoint_config = dict(by_epoch=False, interval=5000, max_keep_ckpts=1)
+evaluation = dict(interval=4000, metric='mIoU')
+# Meta Information for Result Analysis
+name = 'synthia_warm_fdthings_rcs_croppl_a999_daformer_mitb5_s0'
+exp = 'basic'
+name_dataset = 'synthia'
+name_architecture = 'daformer_sepaspp_mitb5'
+name_encoder = 'mitb5'
+name_decoder = 'daformer_sepaspp'
+name_uda = 'dacs_a999_fd_things_rcs0.01_cpl'
+name_opt = 'adamw_6e-05_pmTrue_poly10warm_1x2_40k'
